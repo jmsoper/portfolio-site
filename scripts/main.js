0833f3c2205b8c259713dd19c679be9a728f09a4
jmsoper <jmsoper@gmail.com>
2017-01-08 22:24:40 -0500

    [Projects] Begin implementing mobile animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
 jQuery(document).ready(function($){
+
+	function isElementInViewport (el) {
+
+     //special bonus for those using jQuery
+     if (typeof jQuery === "function" && el instanceof jQuery) {
+         el = el[0];
+     }
+
+     var rect = el.getBoundingClientRect();
+
+     return (
+         rect.top >= 0 &&
+         rect.left >= 0 &&
+         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+         rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+     );
+ }
+
 	var isLateralNavAnimating = false;
 
 	//open/close lateral navigation
